1f953cf491317487c0dca7e292a63838a318ef41
Fyefh <anuarbumble@gmail.com>
2025-01-25 13:16:07 +0500

    added dockerfile for ui

--- internal/ui/src/page/Main.jsx
+++ internal/ui/src/page/Main.jsx
@@ -1,11 +1,12 @@
 import { useEffect, useState } from "react";
+import styles from "../page/Main.module.css";
 
 const Main = () => {
     const [address, setAddress] = useState("");
     const [location, setLocation] = useState(null);
     const [locations, setLocations] = useState([]); // Состояние для всех локаций
+    const [isOpen, setIsOpen] = useState(false);
 
-    // Получение одной локации по IP
     const fetchLocation = async (address) => {
         try {
             const response = await fetch(`
@@ -29,95 +30,110 @@ const Main = () => {
         }
     };
 
-    // Получение всех локаций
+
     const fetchLocations = async () => {
         try {
-            const response = await fetch(`http://localhost:8000/locations`, {
-            method: "GET",
-                headers: {
-                "Content-Type": "application/json",
-            },
-            mode: "cors",
-        });
-        if (!response.ok) {
-            throw new Error("Failed to fetch locations");
-        }
-        const data = await response.json();
-        setLocations(data.result); // Сохраняем список локаций в состояние
-    } catch (error) {
-        console.error(error);
-    }
-};
-
-const handleSearch = async () => {
-    if (!address.trim()) return;
-    const locationData = await fetchLocation(address);
-    setLocation(locationData?.result || null); // Сохраняем результат только из "result"
-};
-
-
-const fetchDefault = async () => {
-    try {
-        const response = await fetch(
-            `http://localhost:8000/location`,
-            {
+            const response = await fetch('http://localhost:8000/locations', {
                 method: "GET",
                 headers: {
                     "Content-Type": "application/json",
                 },
                 mode: "cors",
+            });
+            if (!response.ok) {
+                throw new Error("Failed to fetch locations");
+            }
+            const data = await response.json();
+            setLocations(data.result); // Сохраняем список локаций в состояние
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    const fetchDefault = async () => {
+        try {
+            const response = await fetch(
+                "http://localhost:8000/location",
+            {
+                method: "GET",
+                    headers: {
+                "Content-Type": "application/json",
+            },
+                mode: "cors",
             }
         );
-        if (!response.ok) {
-            throw new Error("Failed to fetch location");
+            if (!response.ok) {
+                throw new Error("Failed to fetch location");
+            }
+            const data = await response.json();
+            setAddress(data.result.query || "")
+            return data
+        } catch (error) {
+            console.error(error);
+            return null;
         }
-        const data = await response.json();
-        return data;
-    } catch (error) {
-        console.error(error);
-        return null;
-    }
-};
+    };
 
-const handleDefault = async () => {
-    if (!address.trim()) return;
-    const locationData = await fetchDefault();
-    setLocation(locationData?.result || null); // Сохраняем результат только из "result"
-};
+    const handleSearch = async () => {
+        if (!address.trim()) return;
+        const locationData = await fetchLocation(address);
+        setLocation(locationData?.result || null); // Сохраняем результат только из "result"
+    };
 
-useEffect(()=> {
-    handleDefault();
-}, [location])
+    const handleDefault = async () => {
+        const locationData = await fetchDefault();
+        setLocation(locationData?.result || null); // Сохраняем результат только из "result"
+    };
 
-return (
-    <div>
-        <input
-            type="text"
-            value={address}
-            onChange={(e) => setAddress(e.target.value)}
+    const handleKeyPress = (e) => {
+        if (e.key === "Enter") { // Проверяем, нажата ли клавиша Enter
+            handleSearch();
+        }
+    };
 
-        />
-        <button onClick={handleSearch}>Search</button>
-        {location && (
-            <div>
-                <pre>{JSON.stringify(location, null, 2)}</pre>
-            </div>
-        )}
+    useEffect(()=> {
+        handleDefault();
+    }, [])
 
-        <button onClick={fetchLocations}>Show All Locations</button>
-        <div>
-            {locations.length > 0 && (
-                <ul>
-                    {locations.map((loc, index) => (
-                        <li key={index}>
-                            <pre>{JSON.stringify(loc, null, 2)}</pre>
-                        </li>
-                    ))}
-                </ul>
-            )}
+    return (
+        <div className={styles.mainPage}>
+            <section>
+                <input
+                    type="text"
+                    value={address}
+                    onChange={(e) => setAddress(e.target.value)}
+                    onKeyDown={handleKeyPress}
+                />
+                <button className={styles.searchBtn} onClick={handleSearch}>Search</button>
+                {location && (
+                    <div className={styles.location}>
+                        <pre>{JSON.stringify(location, null, 2)}</pre>
+                    </div>
+                )}
+            </section>
+
+            <section className={`${isOpen ? styles.show : styles.nope}`}>
+                <div className={styles.buttons}>
+                    <button onClick={fetchLocations}>Show All Locations</button>
+                    <button onClick={()=> setIsOpen(!isOpen)}></button>
+                </div>
+                <div className={styles.locations}>
+                    {locations.length > 0 && (
+                        <div className={styles.location}>
+                            <ul>
+                                {locations.map((loc, index) => (
+                                    <li key={index}>
+                                        <pre>{JSON.stringify(loc, null, 2)}</pre>
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                    )}
+                </div>
+            </section>
+
         </div>
-    </div>
-);
+    );
 };
 
 export default Main;
